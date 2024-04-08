8f6de7cce4720a84c9f2ea9f84189dc0ace570db
ashrakt <ashraktamin678@gmail.com>
2024-04-08 02:21:22 -0700

    first commit

--- app/Http/Controllers/Auth/SocialLoginController.php
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -9,25 +9,32 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
+use Nette\Utils\Random;
 
 class SocialLoginController extends Controller
 {
-    public function redirect($provider){
+    public function redirect($provider)
+    {
         return Socialite::driver($provider)->redirect();
-
     }
 
-    public function callback($provider){
-            $user = Socialite::driver($provider)->user();
-      
- dd($user);
+    public function callback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
         $existingUser = User::where('email', $user->email)->first();
-
         if ($existingUser) {
-
-           Auth::login($existingUser);
+            Auth::login($existingUser);
+        }else{
+            User::create([
+                'name'          => $user->name,
+                'email'         => $user->email,
+                'password'      => Hash::make(Str::random(8)),
+                'provider_name' => $provider,
+                'provider_id'   => $user->id,
+                'provider_token'=> $user->token
+            ]);
 
         }
-        return redirect(route('home'));
+        return view('welcome');
     }
 }
